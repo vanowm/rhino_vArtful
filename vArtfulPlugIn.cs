4522e93d294d301bced6bd6fdad22e0ec3d908bc
V@no <git@vano.org>
2026-05-06 12:26:23 -0400

    26.05.06.122022: initial commit — port Artful.py as standalone vArtful plugin

---
+++ vArtfulPlugIn.cs
@@ -0,0 +1,25 @@
+using Rhino;
+using Rhino.PlugIns;
+
+namespace VArtful;
+
+[System.Runtime.InteropServices.Guid("e65c3282-3c23-408e-901e-86d82dd41070")]
+public sealed class VArtfulPlugIn : PlugIn
+{
+    public override PlugInLoadTime LoadTime => PlugInLoadTime.AtStartup;
+    protected override string LocalPlugInName => "vArtful";
+
+    public VArtfulPlugIn()
+    {
+        Instance = this;
+    }
+
+    public static VArtfulPlugIn Instance { get; private set; } = null!;
+
+    protected override LoadReturnCode OnLoad(ref string errorMessage)
+    {
+        var version = GetType().Assembly.GetName().Version?.ToString() ?? "unknown";
+        RhinoApp.WriteLine($"vArtful v{version} loaded.");
+        return LoadReturnCode.Success;
+    }
+}
