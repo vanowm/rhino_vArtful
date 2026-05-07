0489b26983006b46442e9b3f37c9b452b854b64d
V@no <git@vano.org>
2026-05-07 10:48:28 -0400

    ﻿26.5.7.1029: build: sync version metadata

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using Rhino.PlugIns;
 [assembly: AssemblyProduct("vArtful")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e65c3282-3c23-408e-901e-86d82dd41070")]
-[assembly: AssemblyVersion("26.5.7.956")]
-[assembly: AssemblyFileVersion("26.5.7.956")]
-[assembly: AssemblyInformationalVersion("26.5.7.956")]
+[assembly: AssemblyVersion("26.5.7.1029")]
+[assembly: AssemblyFileVersion("26.5.7.1029")]
+[assembly: AssemblyInformationalVersion("26.5.7.1029")]
 
 [assembly: PlugInDescription(DescriptionType.Organization, "")]
 [assembly: PlugInDescription(DescriptionType.Address, "")]
