781f6149684013874fb4d1577751a46fc898af7c
V@no <git@vano.org>
2026-06-18 12:17:59 -0400

    26.6.18.1215: added option to change layer names

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,13 +3,10 @@ using System.Runtime.InteropServices;
 using Rhino.PlugIns;
 
 [assembly: AssemblyTitle("vArtful")]
-[assembly: AssemblyDescription("Applies the Artful.3dm template to the active document: syncs all document settings, annotation tables, and layers, then organizes layers per the Artful workflow (move objects from numeric layers to '. Proliner .', delete numeric layers, sort to template order, set current layer to '- Traced -').")]
+[assembly: AssemblyDescription("Applies the Artful.3dm template to the active document: syncs document settings, annotation tables, and layers, then organizes layers per the Artful workflow.")]
 [assembly: AssemblyProduct("vArtful")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e65c3282-3c23-408e-901e-86d82dd41070")]
-[assembly: AssemblyVersion("26.5.7.1029")]
-[assembly: AssemblyFileVersion("26.5.7.1029")]
-[assembly: AssemblyInformationalVersion("26.5.7.1029")]
 
 [assembly: PlugInDescription(DescriptionType.Organization, "")]
 [assembly: PlugInDescription(DescriptionType.Address, "")]
