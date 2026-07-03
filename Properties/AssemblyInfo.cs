128400a88182923568e2024ee5fe658bb19c5b4c
V@no <git@vano.org>
2026-07-03 08:45:36 -0400

    Auto versioning

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,10 +3,13 @@ using System.Runtime.InteropServices;
 using Rhino.PlugIns;
 
 [assembly: AssemblyTitle("vArtful")]
-[assembly: AssemblyDescription("Applies the Artful.3dm template to the active document: syncs document settings, annotation tables, and layers, then organizes layers per the Artful workflow.")]
+[assembly: AssemblyDescription("Applies the Artful.3dm template to the active document: syncs all document settings, annotation tables, and layers, then organizes layers per the Artful workflow (move objects from numeric layers to '. Proliner .', delete numeric layers, sort to template order, set current layer to '- Traced -').")]
 [assembly: AssemblyProduct("vArtful")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e65c3282-3c23-408e-901e-86d82dd41070")]
+[assembly: AssemblyVersion("26.5.7.956")]
+[assembly: AssemblyFileVersion("26.5.7.956")]
+[assembly: AssemblyInformationalVersion("26.5.7.956")]
 
 [assembly: PlugInDescription(DescriptionType.Organization, "")]
 [assembly: PlugInDescription(DescriptionType.Address, "")]
